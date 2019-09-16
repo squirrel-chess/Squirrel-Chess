7262c3ee399e828a6e543f8843c894fa8b40f1ca
League Student <league@iMac.attlocal.net>
2019-09-15 17:00:37 -0700

    auto scaling

--- src/Chess.java
+++ src/Chess.java
@@ -1,25 +1,21 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 
 public class Chess {
 	
 	private JFrame frame;
-	private JPanel textPanel;
 	private JLabel text;
 	private Board board;
-	private Dimension frameDim;
-	private int width, height;
 	
 	public Chess() {
 		frame = new JFrame("Squirrel Chess");
 		text = new JLabel();
 		board = new Board(this);
+		frame.setVisible(true);
 		frame.setLayout(new BorderLayout());
 		frame.add(board, BorderLayout.CENTER);
 		frame.setVisible(true);
@@ -27,13 +23,7 @@ public class Chess {
 		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
-		do {
-		width = Integer.parseInt(JOptionPane.showInputDialog("Enter frame width"));
-		height = Integer.parseInt(JOptionPane.showInputDialog("Enter frame height"));
-		frameDim = new Dimension(width, height);
-		frame.setPreferredSize(frameDim);
-		frame.setSize(frameDim);
-		} while (JOptionPane.showConfirmDialog(null, "Is this resolution OK?") != JOptionPane.OK_OPTION);
+		frame.setPreferredSize(new Dimension((int) Toolkit.getDefaultToolkit().getScreenSize().getWidth(), (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight() - 110));
 		board.startTime();
 	}
 	
