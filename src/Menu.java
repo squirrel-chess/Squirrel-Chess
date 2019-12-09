a00b2fd853dc90dd18cba8b2ce03151c1deb4772
League Student <league@iMac-31.attlocal.net>
2019-12-08 16:47:58 -0800

    Finished basic main menu with play working

--- src/Menu.java
+++ src/Menu.java
@@ -12,17 +12,27 @@ public class Menu extends JPanel implements ActionListener{
 	JButton play;
 	JButton instruct;
 	Font titleFont = new Font("Dyeline", Font.ITALIC, 50);
-	public Menu() {
+	public Menu(Chess game) {
+		this.game = game;
+		setLayout(null);
 		setSize(1000, 1000);
 		title = new JLabel();
 		add(title);
 		title.setText("Squirrel Chess");
 		title.setFont(titleFont);
-		title.setLocation(250, 100);
+		title.setBounds(500, 100, 500, 100);
 		play = new JButton();
+		play.setText("Play Game");
+		play.addActionListener((e) -> {
+				this.game.setupGame();
+		});
 		instruct = new JButton();
+		instruct.setText("Instuctions");
+		add(play);
+		play.setBounds(500, 200, 100, 20);
+		add(instruct);
+		instruct.setBounds(500, 300, 100, 20);
 	}
-
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
