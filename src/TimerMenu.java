3197b67b71c164ba4cbd41ea1de00cc96f241183
League Student <league@iMac-28.attlocal.net>
2020-02-23 17:29:50 -0800

    Still working on formatting

--- src/TimerMenu.java
+++ src/TimerMenu.java
@@ -17,6 +17,13 @@ public class TimerMenu extends JPanel implements ActionListener {
 	private JButton nHour;
 	private JButton pHour;
 	private JLabel hour;
+	private int hourNum = 0;
+	private JTextField mins;
+	private JButton nMin;
+	private JButton pMin;
+	private JLabel min;
+	private int minNum = 0;
+	private int yVal = 80;
 
 	public TimerMenu(Chess game) {
 		this.game = game;
@@ -37,19 +44,19 @@ public class TimerMenu extends JPanel implements ActionListener {
 		back.setBounds(40, 400, 100, 40);
 		add(back);
 		hours = new JTextField();
-		hours.setBounds(40, 50, 30, 20);
+		hours.setBounds(50, yVal, 30, 40);
 		add(hours);
 		nHour = new JButton();
 		nHour.addActionListener((e)-> {
-			if (hours.getText().matches("[0-9] + $")) {
-				int i = Integer.parseInt(hours.getText())+1;
-				hours.setText(Integer.toString(i));	
+			if (hours.getText().matches("[0-9]+$") && Integer.parseInt(hours.getText())>0) {
+				int i = Integer.parseInt(hours.getText());
+				i--;
+				hours.setText(""+i);	
 			}else if(hours.getText().isEmpty()) {
 				hours.setText("1");	
 			}
-		
 		});
-		nHour.setBounds(20, 50, 20, 20);
+		nHour.setBounds(20, yVal, 40, 40);
 		try {
 			BufferedImage img = ImageIO.read(this.getClass().getResourceAsStream("leftAr.png"));
 			nHour.setIcon(new ImageIcon(img));
@@ -58,15 +65,67 @@ public class TimerMenu extends JPanel implements ActionListener {
 		}
 		add(nHour);
 		pHour = new JButton();
-		// add action listener to nHour
-		pHour.setBounds(70, 50, 20, 20);
+		// add action listener to pHour(e)-> {
+		pHour.setBounds(70, yVal, 40, 40);
 		try {
 			BufferedImage img = ImageIO.read(this.getClass().getResourceAsStream("rightAr.png"));
 			pHour.setIcon(new ImageIcon(img));
 		} catch (Exception ex) {
 			System.out.println(ex);
 		}
+		pHour.addActionListener((e)-> {
+			if (hours.getText().matches("[0-9]+$") && Integer.parseInt(hours.getText())<60) {
+				hourNum = Integer.parseInt(hours.getText());
+				hourNum++;
+				hours.setText(""+hourNum);	
+			}else if(hours.getText().isEmpty()) {
+				hours.setText("1");	
+			}
+		});
 		add(pHour);
+		
+		mins = new JTextField();
+		mins.setBounds(140, yVal, 30, 40);
+		add(mins);
+		nMin = new JButton();
+		nMin.addActionListener((e)-> {
+			if (mins.getText().matches("[0-9]+$") && Integer.parseInt(mins.getText())>0) {
+				int i = Integer.parseInt(mins.getText());
+				i--;
+				mins.setText(""+i);	
+			}else if(mins.getText().isEmpty()) {
+				mins.setText("1");	
+			}
+		
+		});
+		nMin.setBounds(140, yVal, 40, 40);
+		try {
+			BufferedImage img = ImageIO.read(this.getClass().getResourceAsStream("leftAr.png"));
+			nMin.setIcon(new ImageIcon(img));
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+		add(nMin);
+		pMin = new JButton();
+		pMin.setBounds(170, yVal, 40, 40);
+		try {
+			BufferedImage img = ImageIO.read(this.getClass().getResourceAsStream("rightAr.png"));
+			pMin.setIcon(new ImageIcon(img));
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+		
+		pMin.addActionListener((e)-> {
+			if (mins.getText().matches("[0-9]+$") && Integer.parseInt(mins.getText())<60) {
+				minNum = Integer.parseInt(mins.getText());
+				minNum++;
+				mins.setText(""+minNum);	
+			}else if(mins.getText().isEmpty()) {
+				mins.setText("1");	
+			}
+		
+		});
+		add(pMin);
 		
 	}
 
